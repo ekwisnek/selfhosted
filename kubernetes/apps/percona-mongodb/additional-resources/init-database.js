b76f26e9023ac4d07eb68a3c923fbbfcb9593161
Evan Kwisnek <evankwisnek@gmail.com>
2023-10-31 09:35:15 -0400

    mods

--- kubernetes/apps/percona-mongodb/additional-resources/init-database.js
+++ kubernetes/apps/percona-mongodb/additional-resources/init-database.js
@@ -1,8 +1,5 @@
-const mongoHost = [
-  "mongodb-in-cluster-ps-rs0-0.mongodb-in-cluster-ps-rs0.mongodb.svc.cluster.local",
-  "mongodb-in-cluster-ps-rs0-1.mongodb-in-cluster-ps-rs0.mongodb.svc.cluster.local",
-  "mongodb-in-cluster-ps-rs0-2.mongodb-in-cluster-ps-rs0.mongodb.svc.cluster.local",
-];
+const mongoHost =
+  "mongodb+srv://mongodb-in-cluster-ps-rs0.mongodb-in-cluster-ps-rs0.mongodb.svc.cluster.local";
 const mongoPort = 27017;
 const adminDbName = "admin";
 const adminUser = process.env.MONGODB_DATABASE_ADMIN_USER;
